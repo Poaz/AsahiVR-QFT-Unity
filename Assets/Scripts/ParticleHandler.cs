f5be7c0a259724bdae176e667e4dfa3b3cec0ef3
Poaz <Dines@rxdesign.dk>
2018-04-23 12:01:58 +0200

    .

---
+++ Assets/Scripts/ParticleHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kvant;
+using UnityEngine;
+
+public class ParticleHandler : MonoBehaviour
+{
+
+    public Spray KSpray;
+
+    private Transform _rightHand, _leftHand, _bodyCenter;
+    private float _rightHandToBody, _leftHandToBody;
+    private Vector3 _tmpRightHandPos;
+
+
+    void Start ()
+	{
+	    KSpray = GameObject.FindGameObjectWithTag("Spray").GetComponent<Spray>();
+        _rightHand = GameObject.FindGameObjectWithTag("RightHand").transform;
+	    _bodyCenter = GameObject.FindGameObjectWithTag("BodyCenter").transform;        
+    }
+	
+	void FixedUpdate ()
+	{
+
+        _rightHandToBody = Vector3.Magnitude(_rightHand.position - _tmpRightHandPos);
+	    KSpray.noiseAmplitude = _rightHandToBody*15;
+	    _tmpRightHandPos = _rightHand.position;
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_rightHand.position, _bodyCenter.position);
+    }
+}
