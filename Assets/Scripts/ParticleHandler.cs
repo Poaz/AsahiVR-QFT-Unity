f08845cbb9e3058884321e9a2249a93356e0a5af
Poaz <Dines@rxdesign.dk>
2018-04-25 07:58:35 +0200

    .

--- Assets/Scripts/ParticleHandler.cs
+++ Assets/Scripts/ParticleHandler.cs
@@ -8,8 +8,8 @@ public class ParticleHandler : MonoBehaviour
 
     public Spray KSpray;
 
-    private Transform _rightHand, _leftHand, _bodyCenter;
-    private float _rightHandToBody, _leftHandToBody;
+    private Transform _rightHand, _leftHand, _bodyCenter, _leftFoot, _rightFoot;
+    public float _rightHandToBody, _leftHandToBody, _rightHandToFoot, _leftHandToFoot;
     private Vector3 _tmpRightHandPos;
 
 
@@ -17,20 +17,26 @@ public class ParticleHandler : MonoBehaviour
 	{
 	    KSpray = GameObject.FindGameObjectWithTag("Spray").GetComponent<Spray>();
         _rightHand = GameObject.FindGameObjectWithTag("RightHand").transform;
-	    _bodyCenter = GameObject.FindGameObjectWithTag("BodyCenter").transform;        
+	    _bodyCenter = GameObject.FindGameObjectWithTag("BodyCenter").transform;
+        _rightFoot = GameObject.FindGameObjectWithTag("RightFoot").transform;
+        _leftFoot = GameObject.FindGameObjectWithTag("LeftFoot").transform;
     }
 	
 	void FixedUpdate ()
 	{
 
         _rightHandToBody = Vector3.Magnitude(_rightHand.position - _tmpRightHandPos);
-	    KSpray.noiseAmplitude = _rightHandToBody*15;
+	    KSpray.noiseAmplitude = _rightHandToBody*5;
 	    _tmpRightHandPos = _rightHand.position;
-	}
+
+        _rightHandToFoot = Vector3.Magnitude(_rightHand.position - _rightFoot.position);
+        KSpray.acceleration = new Vector3(0, _rightHandToFoot-1, 0);
+    }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(_rightHand.position, _bodyCenter.position);
+        Gizmos.DrawLine(_rightHand.position, _rightFoot.position);
     }
 }
