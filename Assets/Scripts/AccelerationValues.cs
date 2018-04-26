3c4e64503377de72f81256796944b37e6f3154de
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2018-04-26 08:43:36 +0200

    more stuff

---
+++ Assets/Scripts/AccelerationValues.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationValues : MonoBehaviour
+{
+
+    private Vector3 _rightHand, _leftHand, _bodyCenter, _rightFoot, _leftFoot,
+        _rightHandComparison, _leftHandComparison, _bodyCenterComparison, _rightFootComparison, _leftFootComparison,
+        _rightHandLast, _leftHandLast, _bodyCenterLast, _rightFootLast, _leftFootLast, leftHandDiff, rightHandDiff, leftFootDiff, rightFootDiff;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    _rightHand = GameObject.FindGameObjectWithTag("RightHand").transform.position;
+	    _leftHand = GameObject.FindGameObjectWithTag("LeftHand").transform.position;
+	    _bodyCenter = GameObject.FindGameObjectWithTag("BodyCenter").transform.position;
+	    _rightFoot = GameObject.FindGameObjectWithTag("RightFoot").transform.position;
+	    _leftFoot = GameObject.FindGameObjectWithTag("LeftFoot").transform.position;
+
+	    //_rightHand
+
+        _rightHandComparison = GameObject.FindGameObjectWithTag("RightHand2").transform.position;
+	    _leftHandComparison = GameObject.FindGameObjectWithTag("LeftHand2").transform.position;
+	    _bodyCenterComparison = GameObject.FindGameObjectWithTag("BodyCenter2").transform.position;
+	    _rightFootComparison = GameObject.FindGameObjectWithTag("RightFoot2").transform.position;
+	    _leftFootComparison= GameObject.FindGameObjectWithTag("LeftFoot2").transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+
+	   // rightHandDiff = ;
+
+
+
+	}
+
+}
--- Assets/Scripts/ParticleHandler.cs
+++ Assets/Scripts/ParticleHandler.cs
@@ -9,14 +9,19 @@ public class ParticleHandler : MonoBehaviour
     public Spray KSpray;
 
     private Transform _rightHand, _leftHand, _bodyCenter, _leftFoot, _rightFoot, _particleCenter, _emitter;
-    public float _rightHandToBody, _leftHandToBody, _rightHandToFoot, _leftHandToFoot, _sprayWidth;
-    private Vector3 _tmpRightHandPos, _tmpDirection, _emitterWidth;
-
+    public float _rightHandToBody, _leftHandToBody, _rightHandToFoot, _leftHandToFoot, _sprayWidth, accAmplifier, size;
+    private Vector3 _tmpRightHandPos, _tmpDirection, _emitterWidth, _rightHandVel,_leftHandVel, _rightFootVel, _leftFootVel, _lastHandPos, _distPerFrame, _handPos;
+    public Vector3 _handAcceleration;
+    private Material _sprayMaterial;
+    private Color _color;
 
     void Start ()
 	{
 	    KSpray = GameObject.FindGameObjectWithTag("Spray").GetComponent<Spray>();
 	    _particleCenter = KSpray.transform;
+	    _sprayMaterial = KSpray.material;
+	    _color = _sprayMaterial.color;
+
 	    _emitter = GameObject.FindGameObjectWithTag("Emitter").transform;
         _rightHand = GameObject.FindGameObjectWithTag("RightHand").transform;
 	    _leftHand = GameObject.FindGameObjectWithTag("LeftHand").transform;
@@ -24,28 +29,47 @@ public class ParticleHandler : MonoBehaviour
         _rightFoot = GameObject.FindGameObjectWithTag("RightFoot").transform;
         _leftFoot = GameObject.FindGameObjectWithTag("LeftFoot").transform;
 
-	    _particleCenter.position = _emitter.position;
-	   
+
+        _lastHandPos = Vector3.zero;
+
+        _particleCenter.position = _emitter.position;
+
     }
 
     void FixedUpdate()
     {
+       
 
         _rightHandToBody = Vector3.Magnitude(_rightHand.position - _tmpRightHandPos);
-        // KSpray.noiseAmplitude = _rightHandToBody*5;
+       // KSpray.noiseAmplitude = _rightHandToBody*5;
         _tmpRightHandPos = _rightHand.position;
 
         _rightHandToFoot = Vector3.Magnitude(_rightHand.position - _rightFoot.position);
-        //   KSpray.acceleration = new Vector3(0, _rightHandToFoot-1, 0);
+        // KSpray.acceleration = new Vector3(0, _rightHandToFoot-1, 0);
+
+
 
         //direction of spray
         _tmpDirection = (_rightHand.position + _leftHand.position) / 2 - _bodyCenter.position;
-        KSpray.acceleration = _tmpDirection;
+        KSpray.initialVelocity = _tmpDirection*accAmplifier;
 
         //width of spray
         _sprayWidth = Vector3.Distance(_leftHand.position, _rightHand.position);
-        _emitterWidth = new Vector3(_sprayWidth,0.1f,0.1f);
-        KSpray.SetEmitterSize(_emitterWidth);
+        _emitterWidth = new Vector3(_sprayWidth,0.1f,0.1f)* size;
+        KSpray.directionSpread = _sprayWidth*size;
+        //KSpray.SetEmitterSize(_emitterWidth);
+
+        //acceleration vector
+        _handPos = (_rightHand.position + _leftHand.position)/2;
+        _distPerFrame = _handPos - _lastHandPos;
+        _lastHandPos = _handPos;
+        _handAcceleration = _distPerFrame * Time.deltaTime;
+        
+
+        //material color
+        _color += new Color(_handAcceleration.x,_handAcceleration.y,_handAcceleration.z);
+        //_sprayMaterial.color = _color;
+        
     }
 
     void OnDrawGizmos()
